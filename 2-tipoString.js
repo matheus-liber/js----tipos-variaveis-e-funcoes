46c678e45ae6320938d8681672da051762544777
matheus-liber <matheuslibergt@gmail.com>
2023-01-09 15:41:59 -0300

    exemplos da aula 1

---
+++ 2-tipoString.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaTopDasGalaxias!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi!"';
+console.log(citacao);
+
+const fraseNome = "Meu nome é ";
+const meuNome = "Matheus"
+
+//concatenação (+)
+
+console.log(fraseNome + meuNome);
+
+//template string OU template literal
+
